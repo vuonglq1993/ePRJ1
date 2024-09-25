fdcc4e86caf4251daf17824b066198b937ae3ae2
ThaiChuT9 <iamtk4i@gmail.com>
2024-09-25 09:06:42 +0700

    Update db.php

---
+++ functions/db.php
@@ -0,0 +1,9 @@
+<?php
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "bidspirit";
+$conn = new mysqli($host, $user, $password, $database);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
