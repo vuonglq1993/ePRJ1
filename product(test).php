e1e8865d9f8ac85e9b5f0d51ac28737c8e135a46
ThaiChuT9 <iamtk4i@gmail.com>
2024-09-26 11:03:22 +0700

    update title

--- product(test).php
+++ product(test).php
@@ -17,7 +17,7 @@
     ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo htmlspecialchars($product['product_name']); ?> - Product Details</title>
+    <title>Doc</title>
 </head>
 <body>
     <h1>Products in Category</h1>
--- product-detail(test).php
+++ product-detail(test).php
@@ -16,7 +16,7 @@
     ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title><?php echo htmlspecialchars($product['product_name']); ?> </title>
 </head>
 
 <body>
