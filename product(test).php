f94e6dda221e2e45fce91b3eba43a967e70fe3fd
ThaiChuT9 <iamtk4i@gmail.com>
2024-09-26 11:08:27 +0700

    starting price, buy now price

--- product(test).php
+++ product(test).php
@@ -27,7 +27,8 @@
         <?php foreach ($products as $product): ?>
             <div class="product-item">
                 <h2><?php echo htmlspecialchars($product['product_name']); ?></h2>
-                <p>Price: $<?php echo number_format($product['starting_price'], 2); ?></p>
+                <p>Starting Price: $<?php echo number_format($product['starting_price'], 2); ?></p>
+                <p>Buy Now Price: $<?php echo number_format($product['buyout_price'], 2); ?></p>
                 <p>Description: <?php echo htmlspecialchars($product['description']); ?></p>
                 <p>
                     <img src="<?php echo htmlspecialchars($product['image_url']); ?>" alt="Product Image" style="max-width: 200px;">
--- product-detail(test).php
+++ product-detail(test).php
@@ -22,7 +22,8 @@
 <body>
     <h1>Product Details</h1>
     <p>Product Name: <?php echo htmlspecialchars($product['product_name']); ?></p>
-    <p>Product Price: $<?php echo number_format($product['starting_price'], 2); ?></p>
+    <p>Product Starting Price: $<?php echo number_format($product['starting_price'], 2); ?></p>
+    <p>Product Buy Now Price: $<?php echo number_format($product['buyout_price'], 2); ?></p>
     <p>Product Description: <?php echo htmlspecialchars($product['description']); ?></p>
     <p>Product Image: <img src="<?php echo htmlspecialchars($product['image_url']); ?>" alt="Product Image" style="max-width: 200px;"></p>
     <a href="product(test).php?cat_id=<?php echo $product['category_id']; ?>">Back to Products</a>
