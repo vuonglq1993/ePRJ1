41cef705500aeb4d1993597f4f1402d3f1a1c934
VuongLe <lqv1993@gmail.com>
2024-09-27 14:25:14 +0700

    Bảng user_likes

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
         integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
         crossorigin="anonymous"></script>
-    <script src="js/index.js"></script>
+    <script src="javascript/index.js"></script>
     <title>Home</title>
 </head>
 
