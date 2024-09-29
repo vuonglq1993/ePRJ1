6cdaba06da87e3d87904b2d01edbf397b7c6c4f9
Huy Dopin <95194108+Pindos22@users.noreply.github.com>
2024-09-30 00:27:20 +0700

    ahjrljql

---
+++ Components/header.md
@@ -0,0 +1,3 @@
+<link rel="stylesheet" href="Components/header.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" />
+<?php include 'Components/header.php'; ?>
--- Components/header.php
+++ Components/header.php
@@ -1,1 +1,43 @@
-<?php echo"asjkbfabw" ?>
+<header>
+    <div class="headerr pt-2 pb-2 row">
+        <div class="col-md-2 d-flex justify-content-center align-items-center">
+            <img src="../images/header/Group.png" alt="" class="logo-img">
+        </div>
+        <div class="d-flex col-md-4">
+            <ul class="header-links-list d-flex flex-grow-1">
+                <li class="flex-fill"><a href="">Categories</a></li>
+                <li class="flex-fill"><a href="">Popular Artists</a></li>
+                <li class="flex-fill"><a href="">Inspire me</a></li>
+            </ul>
+            </div>
+            <div class="d-flex col-md-4">
+            <div class="input-group ms-3">
+                <span class="input-group-text border-0"><i class="bi bi-search text-muted"></i></span>
+                <input class="form-control border-0" type="search" placeholder="Tìm kiếm..." aria-label="Search">
+            </div>
+            <button class="header-btn ms-2" type="submit">Sell my Art</button>
+        </div>
+        <div class="d-flex header-icon-list col-md-2 justify-content-between">
+            <ul class="d-flex list-unstyled m-0 flex-grow-1 justify-content-end">
+                <li class="me-3">
+                    <a href=""><i class="fa-solid fa-user fa-2x"></i></a>
+                </li>
+                <li class="position-relative me-3">
+                    <a href="#">
+                        <i class="fas fa-heart fa-2x"></i>
+                        <span
+                            class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">1</span>
+                    </a>
+                </li>
+                <li class="position-relative">
+                    <a href="#">
+                        <i class="fas fa-shopping-cart fa-2x"></i>
+                        <span
+                            class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">0</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+    </div>
+</header>
