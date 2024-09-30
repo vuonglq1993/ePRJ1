336bf158837d0eb2522a16ca813ed4c8870cd9ea
bemixzyy <bolunlun1210@gmail.com>
2024-09-30 23:35:34 +0700

    ok bro

---
+++ fav_obj.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <link rel="stylesheet" href="style/index.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
+        integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
+        crossorigin="anonymous"></script>
+    <script src="js/index.js"></script>
+    <title>Home</title>
+</head>
+<body>
+<div class="container mt-5">
+            <div class="row justify-content-center">
+                <div class="col-11">
+                    <div class="row justify-content-center">
+                        <div class="d-flex flex-row">
+                            <div class="p-2">
+                                <p class="fs-3">My favourites</p>
+                                <nav class="nav">
+                                    <a class="nav-link" href="#">Objects</a>
+                                    <a class="nav-link active" aria-current="page" href="#">Collections</a>
+                                    <a class="nav-link" href="#">Seller</a>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>  
+            </div>
+</div>
+</body>
+</html>
