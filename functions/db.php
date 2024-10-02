6a74e6b13ff68c8bd3f032dbbcd3df33e11cd05f
ThaiChuT9 <iamtk4i@gmail.com>
2024-10-02 23:19:39 +0700

    Update db.php

--- functions/db.php
+++ functions/db.php
@@ -2,8 +2,8 @@
 function connect(){
     $host = "localhost";
     $user = "root";
-    $password = "root";
-    $database = "bidspirit";
+    $password = "";
+    $database = "test";
     $conn = new mysqli($host, $user, $password, $database);
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
@@ -11,16 +11,31 @@ function connect(){
     return $conn;
 }
 
-function select($sql){
+function select($sql, $params = []){
     $conn = connect();
-    $result = $conn->query($sql);
+    if(!$conn){
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    if (!empty($params)) {
+        $stmt = $conn->prepare($sql);
+        if ($stmt === false) {
+            die("Error preparing statement: " . $conn->error);
+        }
+        $types = str_repeat('s', count($params));
+        $stmt->bind_param($types, ...$params);
+        $stmt->execute();
+        $result = $stmt->get_result();
+    } else {
+        $result = $conn->query($sql);
+    }
+    if ($result === false) {
+        die("Error executing query: " . $conn->error);
+    }
     $data = [];
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
             $data[] = $row;
         }
-    } else {
-        $data = 0;
     }
     $conn->close();
     return $data;
