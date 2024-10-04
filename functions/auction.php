2836179e0ff98bc20af9aec4b6b0447f7309e334
ThaiChuT9 <iamtk4i@gmail.com>
2024-10-04 16:46:38 +0700

    Update auction.php

--- functions/auction.php
+++ functions/auction.php
@@ -4,18 +4,18 @@ include_once 'functions/db.php';
 function get_auction_data($user_id, $category_id = 0)
 {
     if ($category_id > 0) {
-        $sql = "SELECT a.product_id, a.current_bid, a.start_time, a.end_time, p.product_name, p.image_url, ul.id AS liked
+        $sql = "SELECT a.product_id, a.current_bid, a.start_time, a.end_time, p.product_name, p.buyout_price, p.image_url, ul.id AS liked
                 FROM auctions a
-                JOIN products p ON a.product_id = p.product_id
+                RIGHT JOIN products p ON a.product_id = p.product_id
                 LEFT JOIN user_likes ul ON a.product_id = ul.product_id AND ul.user_id = ?
                 WHERE p.category_id = $category_id
-                ORDER BY a.start_time ASC";
+                ORDER BY CASE WHEN a.start_time IS NULL THEN 1 ELSE 0 END, a.start_time ASC";
     } else {
-        $sql = "SELECT a.product_id, a.current_bid, a.start_time, a.end_time, p.product_name, p.image_url, ul.id AS liked
+        $sql = "SELECT a.product_id, a.current_bid, a.start_time, a.end_time, p.product_name, p.buyout_price, p.image_url, ul.id AS liked
                 FROM auctions a
-                JOIN products p ON a.product_id = p.product_id
+                RIGHT JOIN products p ON a.product_id = p.product_id
                 LEFT JOIN user_likes ul ON a.product_id = ul.product_id AND ul.user_id = ?
-                ORDER BY a.start_time ASC";
+                ORDER BY CASE WHEN a.start_time IS NULL THEN 1 ELSE 0 END, a.start_time ASC";
     }
     return select($sql, [$user_id]);
 }
@@ -28,13 +28,16 @@ function caculate_days_left($start_time, $end_time)
     $start_date_time = new DateTime($start_time);
     $end_date_time = new DateTime($end_time);
     $now = new DateTime();
-    if ($now < $start_date_time) {
-        return "Auction starts in " . $start_date_time->diff($now)->format('%a days');
+    if (empty($start_time)) {
+        return "The auction has not started.";
+    } else if ($now < $start_date_time) {
+        return "Auction starts in " . $start_date_time->diff($now)->format('%a days.');
     } elseif ($now > $end_date_time) {
-        return "Auction ended" . $now->diff($end_date_time)->format('%a days ago');
+        return "Auction ended" . $now->diff($end_date_time)->format('%a days ago.');
     } else {
-        return "Auction ends in " . $now->diff($end_date_time)->format('%a days');
+        return "Auction ends in " . $now->diff($end_date_time)->format('%a days.');
     }
+    
 }
 function get_trendin_data( $user_id)
 {
