f042328084557109154017cca579b6c69cf16c5f
ThaiChuT9 <iamtk4i@gmail.com>
2024-10-04 16:46:47 +0700

    Update fsearch.php

--- functions/fsearch.php
+++ functions/fsearch.php
@@ -2,9 +2,9 @@
     include_once 'db.php';
 function search($searchTerm, $user_id){
     $conn = connect();
-    $sql = "SELECT a.product_id, a.current_bid, a.start_time, a.end_time, p.product_name, p.image_url, ul.id AS liked
+    $sql = "SELECT a.product_id, a.current_bid, a.start_time, a.end_time, p.product_name, p.buyout_price p.image_url, ul.id AS liked
             FROM auctions a
-            JOIN products p ON a.product_id = p.product_id
+            RIGHT JOIN products p ON a.product_id = p.product_id
             LEFT JOIN user_likes ul ON p.product_id = ul.product_id AND ul.user_id = ?
             WHERE p.product_name LIKE ? 
             ORDER BY a.start_time ASC";
@@ -35,11 +35,13 @@ function caculate_days_left_search($start_time, $end_time)
     $start_date_time = new DateTime($start_time);
     $end_date_time = new DateTime($end_time);
     $now = new DateTime();
-    if ($now < $start_date_time) {
-        return "Auction starts in " . $start_date_time->diff($now)->format('%a days');
+    if (empty($start_time)) {
+        return "The auction has not started.";
+    } else if ($now < $start_date_time) {
+        return "Auction starts in " . $start_date_time->diff($now)->format('%a days.');
     } elseif ($now > $end_date_time) {
-        return "Auction ended" . $now->diff($end_date_time)->format('%a days ago');
+        return "Auction ended" . $now->diff($end_date_time)->format('%a days ago.');
     } else {
-        return "Auction ends in " . $now->diff($end_date_time)->format('%a days');
+        return "Auction ends in " . $now->diff($end_date_time)->format('%a days.');
     }
 }
